034d99833b1de43e7267c76e1fba838148b74962
SauloFilho <saulosilvaf@gmail.com>
2019-08-30 18:41:52 -0300

    custom
    products;

--- DS-script.js
+++ DS-script.js
@@ -5,7 +5,7 @@ jQuery(document).ready(function($) {
 		get: 'user',
 		userId: '194830405',
 		limit: 9,
-		accessToken: '194830405.1677ed0.42e36ffaaaa64d458f841c0b70cf10b3',
+		accessToken: '194830405.1677ed0.d26a030b52a14b57bf1f8489b109fb34',
 		resolution: 'standard_resolution',
 		target: 'instafeed',
 		template: '<div class="insta-img-container"><a class="insta-link" href="{{link}}" target="_blank" title="{{caption}}"><img class="insta-img" src="{{image}}"/></a></div>'
