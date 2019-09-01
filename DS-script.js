9e2f476dfe9c0c0b612b682eca587beca0bd5483
SauloFilho <saulosilvaf@gmail.com>
2019-08-31 22:17:35 -0300

    up mini cart
    sat 31 aug 22h17;

--- DS-script.js
+++ DS-script.js
@@ -12,15 +12,3 @@ jQuery(document).ready(function($) {
 	});
 	userFeed.run();
 });
-
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sw.js')
-        .then(registration => {
-          console.log(`Service Worker registered! Scope: ${registration.scope}`);
-        })
-        .catch(err => {
-          console.log(`Service Worker registration failed: ${err}`);
-        });
-    });
-  }
